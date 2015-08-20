d618bd2d7247fe649e709a58b85c1f50667ec648
Julie Heidt <julie.a.heidt@gmail.com>
2015-08-19 17:43:32 -0700

    revised JQuery

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-var input = document.querySelectorAll("innnerBox")
+var turn=0;
+
+
+$(".square").on("click", function(){
+	console.log(this);
+	if (turn===1) {
+		$(this).html("X");
+	}else{
+		$(this).html("O");
+	}
+	turn=turn===0?1:0;
+	
+});
+$("#reset").on("click", function(){
+	console.log(this);
+	$(".square").empty();
+});
+
