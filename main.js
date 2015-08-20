ac4fc25745c6cea3efd9c63868a3b5b209e56805
Julie Heidt <julie.a.heidt@gmail.com>
2015-08-19 19:28:50 -0700

    preventing player from selecting taken box place

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
+
 var turn=0;
-
-
+var squareA
 $(".square").on("click", function(){
-	console.log(this);
-	if (turn===1) {
-		$(this).html("X");
-	}else{
-		$(this).html("O");
+	if($(this).text() !== ""){
+		alert("Choose another box!");
+	} else {	
+		if (turn===1) {
+			$(this).html("X");
+		} else {
+			$(this).html("O");
+		}
+		turn=turn===0?1:0;
 	}
-	turn=turn===0?1:0;
-	
 });
 $("#reset").on("click", function(){
 	console.log(this);
 	$(".square").empty();
 });
 
+wins: [ [ 0, 1, 2 ], [ 3, 4, 5 ], [ 6, 7, 8 ], [ 0, 3, 6 ], [ 1, 4, 7 ], [ 2, 5, 8 ], [ 0, 4, 8 ], [ 2, 4, 6 ] ]
